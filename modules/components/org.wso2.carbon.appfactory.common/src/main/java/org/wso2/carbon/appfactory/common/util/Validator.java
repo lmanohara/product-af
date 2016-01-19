f74daab23ef7967a631d04fc3ef79693b7271f22
Lahiru Manohara <lahiruma@wso2.com>
2016-01-19 14:18:38 +0530

    Validating application name in jaggery block layer

---
+++ modules/components/org.wso2.carbon.appfactory.common/src/main/java/org/wso2/carbon/appfactory/common/util/Validator.java
@@ -0,0 +1,24 @@
+package org.wso2.carbon.appfactory.common.util;
+
+/**
+ * Utilities for validation
+ */
+public class Validator {
+
+    /**
+     * Check whether applicationName name includes special chars
+     *
+     * @param applicationName name of the applicationName
+     * @return applicationName name valid or invalid
+     */
+    public boolean validateApplicationName(String applicationName) {
+        String specialCharPattern = ".*\\W";
+        Boolean isValid;
+        boolean isInvalidChar = applicationName.matches(specialCharPattern);
+        isValid = !isInvalidChar;
+
+        return isValid;
+
+    }
+
+}
