75ae1cd514fe97b9c76a320d133084d019688943
Lahiru Manohara <lahiruma@wso2.com>
2016-01-22 15:29:01 +0530

    General validation methods for jaggery block layer

--- modules/components/org.wso2.carbon.appfactory.common/src/main/java/org/wso2/carbon/appfactory/common/util/Validator.java
+++ modules/components/org.wso2.carbon.appfactory.common/src/main/java/org/wso2/carbon/appfactory/common/util/Validator.java
@@ -6,19 +6,33 @@ package org.wso2.carbon.appfactory.common.util;
 public class Validator {
 
     /**
-     * Check whether applicationName name includes special chars
+     * Check whether input is including invalid chars
      *
      * @param applicationName name of the applicationName
-     * @return applicationName name valid or invalid
+     * @return the input string is valid or invalid
      */
-    public static boolean validateApplicationName(String applicationName) {
-        String specialCharPattern = ".*\\W";
-        Boolean isValid;
-        boolean isInvalidChar = applicationName.matches(specialCharPattern);
-        isValid = !isInvalidChar;
+    public static boolean validateInput(String applicationName) {
+        String allowedCharPattern = ".*\\W";
+        boolean isValidChar = applicationName.matches(allowedCharPattern);
+        return isValidChar;
 
+    }
+
+    /**
+     * Check whether application version is valid
+     *
+     * @param applicationVersion version of the application
+     * @return application version is valid or not
+     */
+    public static boolean validateApplicationVersion(String applicationVersion) {
+        String validNewVersionPattern = "^(\\d{1,2}\\.){2}(\\d{1,5})$";
+        boolean isValid = false;
+        boolean isValidApplicationVersion = validateInput(applicationVersion);
+        //check either application is mach the patter or default version(trunk)
+        if (applicationVersion.matches(validNewVersionPattern) || isValidApplicationVersion) {
+            isValid = true;
+        }
         return isValid;
-
     }
 
 }
